2fd199f9f80ae4bb5c822e4bcd0e4d5c8d8e201a
Nadejda Fedorenko <nadyushka.f@gmail.com>
2021-12-07 21:48:46 -0500

    add negative tests for auth spec

--- pages/login.page.js
+++ pages/login.page.js
@@ -1,9 +1,12 @@
 import Page from './page';
 
 class LoginPage extends Page {
-    get inputUsername () { return $('[qa-id="email"]') }
-    get inputPassword () { return $('[qa-id="password"]') }
-    get buttonSubmit () { return $('.login-form-button') }
+    get inputUsername() { return $('[qa-id="email"]') }
+    get inputPassword() { return $('[qa-id="password"]') }
+    get buttonSubmit() { return $('.login-form-button') }
+    get notification() { return $('.ant-notification-topRight') }
+    get loginError() { return $('//div[@data-qa="normalEmail"]//div[@class="ant-form-item-explain-error"]') }
+    get passwordError() { return $('//div[div[@class="ant-form-item-control-input"]//input[@qa-id="password"]]//div[@class="ant-form-item-explain-error"]') }
 
     async login(username, password) {
         await this.inputUsername.setValue(username);
