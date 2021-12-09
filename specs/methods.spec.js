ac5ea1404ffc568ec5889a7d9279ab80d4abbd0c
Nadejda Fedorenko <nadyushka.f@gmail.com>
2021-12-09 18:48:04 -0500

    add scrolling spec

---
+++ specs/methods.spec.js
@@ -0,0 +1,28 @@
+
+describe('Scrolling', function() {
+    beforeEach(async function() {
+        await browser.url('https://bormando.github.io/pagescrolling/');
+    });
+
+    it('to tap', async function() {
+        const elem = await $('button');
+        const header = await $('.navbar.top');
+        await browser.execute((elem, header) => {
+            window.scrollTo(
+                0,
+                elem.offsetTop- header.offsetHeight
+            )
+        }, elem, header);
+        await elem.click();
+    });
+
+    it('to bottom', async function() {
+        const elem = await $('button');
+        const footer = await $('.navbar.bottom');
+        await elem.scrollIntoView(false);
+        await browser.execute((footer) => {
+            window.scrollTo(0, footer.offsetHeight)
+        }, footer);
+        await elem.click();
+    });
+})
